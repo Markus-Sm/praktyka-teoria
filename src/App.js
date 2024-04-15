f4ff4dbdc4d49d5b6cd4edf26a368ec7b80036ec
Marek Samofal <marek.samofal@gmail.com>
2024-04-15 11:18:33 +0200

    The render Phase

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function TabContent({ item }) {
 
 			<div className='tab-undo'>
 				<button>Undo</button>
-				<button>Undo in 2s</button>
+				<button>Undo in 2s j</button>
 			</div>
 		</div>
 	)
